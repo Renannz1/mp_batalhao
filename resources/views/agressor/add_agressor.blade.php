e826a6282b3bf308e21162a528135f57d8f7f320
Renan Magalhaes <renanpereirareis@gmail.com>
2025-02-07 19:44:49 -0300

    feat: adiciona 'value old' no formulario de criação do agressor

--- resources/views/agressor/add_agressor.blade.php
+++ resources/views/agressor/add_agressor.blade.php
@@ -8,13 +8,15 @@
         <h3>Dados do Agressor</h3>
 
         <label for="nome">Nome: </label>
-        <input type="text" name="nome" id="nome" required><br><br>
+        <input type="text" name="nome" id="nome" value="{{ old('nome') }}" required>
+        <br><br>
         @error('nome')
             <div>{{ $message }}</div>
         @enderror
 
         <label for="idade">Idade: </label>
-        <input type="text" name="idade" id="idade"><br><br>
+        <input type="text" name="idade" id="idade" value="{{ old('idade') }}">
+        <br><br>
         @error('idade')
             <div>{{ $message }}</div>
         @enderror
@@ -24,19 +26,22 @@
         <h3>Endereço do Agressor</h3>
 
         <label for="logradouro">Logradouro: </label>
-        <input type="text" name="logradouro" id="logradouro" required><br><br>
+        <input type="text" name="logradouro" id="logradouro" value="{{ old('logradouro') }}" required>
+        <br><br>
         @error('logradouro')
             <div>{{ $message }}</div>
         @enderror
 
         <label for="bairro">Bairro: </label>
-        <input type="text" name="bairro" id="bairro" required><br><br>
+        <input type="text" name="bairro" id="bairro" value="{{ old('bairro') }}" required>
+        <br><br>
         @error('bairro')
             <div>{{ $message }}</div>
         @enderror
 
         <label for="municipio">Município: </label>
-        <input type="text" name="municipio" id="municipio" required><br><br>
+        <input type="text" name="municipio" id="municipio" value="{{ old('municipio') }}" required>
+        <br><br>
         @error('municipio')
             <div>{{ $message }}</div>
         @enderror
