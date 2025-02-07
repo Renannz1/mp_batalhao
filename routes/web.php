5adb0b0fd3b6a7ab7aef237c4a7c9dccb4cce496
Renan Magalhaes <renanpereirareis@gmail.com>
2025-02-07 19:37:25 -0300

    refactor: altera a escrita das rotas

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AssistidaController::class)->group(function(){
     Route::get('/assistidas', 'listarAssistidas')->name('listar-assistidas');
-    Route::get('/assistidas/form-nova-assistida', 'formNovaAssistida')->name('form-nova-assistida');
-    Route::post('/assistidas/criar-assistida', 'criarAssistida')->name('criar-assistida');
+    Route::get('/assistidas/form-criar', 'formNovaAssistida')->name('form-nova-assistida');
+    Route::post('/assistidas/criar', 'criarAssistida')->name('criar-assistida');
     Route::get('/assistida/{id}', 'detalharAssistida')->name('detalhar-assistida');
-    Route::get('/assistida/form-editar-assistida/{id}', 'formEditarAssistida')->name('editar-assistida');
-    Route::put('/assistida/atualizar-assistida/{id}', 'atualizarAssistida')->name('atualizar-assistida');
-    Route::get('/assistidas/excluir/{id}', 'excluirAssistida')->name('excluir-assistida');
-    Route::get('/assistidas/confirm-excluir/{id}', 'confirmExcluirAssistida')->name('confirm-excluir-assistida');
+    Route::get('/assistida/form-editar/{id}', 'formEditarAssistida')->name('editar-assistida');
+    Route::put('/assistida/atualizar/{id}', 'atualizarAssistida')->name('atualizar-assistida');
+    Route::get('/assistidas/form-excluir/{id}', 'excluirAssistida')->name('excluir-assistida');
+    Route::get('/assistidas/excluir/{id}', 'confirmExcluirAssistida')->name('confirm-excluir-assistida');
 });
 
 Route::controller(AgressorController::class)->group(function(){
     Route::get('/agressores', 'listarAgressores')->name('listar-agressores');
-    Route::get('/agressor/form-novo-agressor', 'formNovoAgressor')->name('form-novo-agressor');
-    Route::post('/agressor/criar-agressor', 'criarAgressor')->name('criar-agressor');
+    Route::get('/agressor/form-criar', 'formNovoAgressor')->name('form-novo-agressor');
+    Route::post('/agressor/criar', 'criarAgressor')->name('criar-agressor');
     Route::get('/agressor/{id}', 'detalharAgressor')->name('detalhar-agressor');
-    Route::get('/agressor/form-editar-agressor/{id}', 'formEditarAgressor')->name('form-editar-agressor');
-    Route::put('/agressor/atualizar-agressor/{id}', 'atualizarAgressor')->name('atualizar-agressor');
-
-    Route::get('/agressor/excluir/{id}', 'excluirAgressor')->name('excluir-agressor');
-    Route::get('/agressor/confirm-excluir/{id}', 'confirmExcluirAgressor')->name('confirm-excluir-agressor');
+    Route::get('/agressor/form-editar/{id}', 'formEditarAgressor')->name('form-editar-agressor');
+    Route::put('/agressor/atualizar/{id}', 'atualizarAgressor')->name('atualizar-agressor');
+    Route::get('/agressor/form-excluir/{id}', 'excluirAgressor')->name('excluir-agressor');
+    Route::get('/agressor/excluir/{id}', 'confirmExcluirAgressor')->name('confirm-excluir-agressor');
 });
