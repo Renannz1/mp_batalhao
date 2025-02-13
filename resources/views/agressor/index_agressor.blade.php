8dff9a5dfa26f9e953c895db91f8febd43c89afa
Renan Magalhães <124892507+Renannz1@users.noreply.github.com>
2025-02-13 00:44:46 -0300

    style: estiliza a view index_agressor

--- resources/views/agressor/index_agressor.blade.php
+++ resources/views/agressor/index_agressor.blade.php
@@ -1,34 +1,79 @@
 @extends('layouts.main_layout')
 
 @section('content')
-    
-<div>
-    <h1>Listando os dados de todos Agressores</h1>
-    <table>
-        <thead class="table">
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($agressores as $agressor)
-            <tr>
-                <td>{{ $agressor->id }}</td>
-                <td>{{ $agressor->nome }}</td>
-                <td><button><a href="{{ route('detalhar-agressor', ['id' => $agressor->id]) }}">Detalhes</a></button></td>
-                <td><button><a href="{{ route('form-editar-agressor', ['id' => $agressor->id]) }}">Editar</a></button></td>
-                <td><button><a href="{{ route('excluir-agressor', ['id' => $agressor->id]) }}">Excluir</a></button></td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
-<div>
-    <br><button><a href="{{ route('form-novo-agressor') }}">Adicionar agressor</a></button>
+
+<div class="container mb-5 mt-4">
+    <div>
+        <h2>Tabela de Agressores</h2>
+        <div class="d-flex justify-content-between align-items-center">
+            <p class="mb-5">Listando todos agressores.</p>
+            <a href="{{ route('form-novo-agressor') }}" class="btn btn-primary mb-5">Cadastrar Novo Agressor</a>
+        </div>
+    </div> 
+
+    <div class="card mb-4">
+        <div class="card-body">
+
+            <table id="datatablesSimple">
+
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Idade</th>
+                        <th>Telefone</th>
+                        <th>Logradouro</th>
+                        <th>Número</th>
+                        <th>Quadra</th>
+                        <th>Bloco</th>
+                        <th>Apartamento</th>
+                        <th>Bairro</th>
+                        <th>Complemento</th>
+                        <th>Munícipio</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Idade</th>
+                        <th>Telefone</th>
+                        <th>Logradouro</th>
+                        <th>Número</th>
+                        <th>Quadra</th>
+                        <th>Bloco</th>
+                        <th>Apartamento</th>
+                        <th>Bairro</th>
+                        <th>Complemento</th>
+                        <th>Munícipio</th>
+                        <th>Ação</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    @foreach ($agressores as $agressor)
+                    <tr>
+                        <td>{{ $agressor->id }}</td>
+                        <td>{{ $agressor->nome }}</td>
+                        <td>{{ $agressor->idade }}</td>
+                        <td>{{ $agressor->telefone }}</td>
+                        <td>{{ $agressor->logradouro }}</td>
+                        <td>{{ $agressor->numero }}</td>
+                        <td>{{ $agressor->quadra }}</td>
+                        <td>{{ $agressor->bloco }}</td>
+                        <td>{{ $agressor->apartamento }}</td>
+                        <td>{{ $agressor->bairro }}</td>
+                        <td>{{ $agressor->complemento }}</td>
+                        <td>{{ $agressor->municipio }}</td>
+                        <td>
+                            <a href="{{ route('detalhar-agressor', ['id' => $agressor->id]) }}" class="">Detalhar</a>
+                        </td>
+                    </tr> 
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 @endsection
-
-
-
